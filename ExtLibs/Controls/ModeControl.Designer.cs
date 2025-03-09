50669a96cd4a9eeaf8b4a470f98a09f62b028aa9
pii19 <piichan19@gmail.com>
2025-03-10 08:32:17 +0900

    mode contorol temp

--- ExtLibs/Controls/ModeControl.Designer.cs
+++ ExtLibs/Controls/ModeControl.Designer.cs
@@ -28,83 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnLOITER = new System.Windows.Forms.Button();
+            this.btnAUTO = new System.Windows.Forms.Button();
             this.labelMode = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button2
+            // btnLOITER
             // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.BackgroundImage = global::MissionPlanner.Controls.Properties.Resources.mode_loiter_off;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F);
-            this.button2.ForeColor = System.Drawing.Color.Transparent;
-            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button2.Location = new System.Drawing.Point(244, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 50);
-            this.button2.TabIndex = 9;
-            this.button2.TabStop = false;
-            this.button2.Tag = "custom";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnLOITER.BackColor = System.Drawing.Color.Transparent;
+            this.btnLOITER.BackgroundImage = global::MissionPlanner.Controls.Properties.Resources.mode_loiter_off;
+            this.btnLOITER.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnLOITER.FlatAppearance.BorderSize = 0;
+            this.btnLOITER.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnLOITER.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnLOITER.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLOITER.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F);
+            this.btnLOITER.ForeColor = System.Drawing.Color.Transparent;
+            this.btnLOITER.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnLOITER.Location = new System.Drawing.Point(229, 0);
+            this.btnLOITER.Name = "btnLOITER";
+            this.btnLOITER.Size = new System.Drawing.Size(121, 50);
+            this.btnLOITER.TabIndex = 9;
+            this.btnLOITER.TabStop = false;
+            this.btnLOITER.Tag = "custom";
+            this.btnLOITER.UseVisualStyleBackColor = false;
+            this.btnLOITER.Click += new System.EventHandler(this.btnLOITER_Click);
             // 
-            // button3
+            // btnAUTO
             // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.BackgroundImage = global::MissionPlanner.Controls.Properties.Resources.mode_auto_on;
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F);
-            this.button3.ForeColor = System.Drawing.Color.Transparent;
-            this.button3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button3.Location = new System.Drawing.Point(123, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 50);
-            this.button3.TabIndex = 8;
-            this.button3.TabStop = false;
-            this.button3.Tag = "custom";
-            this.button3.UseVisualStyleBackColor = false;
+            this.btnAUTO.BackColor = System.Drawing.Color.Transparent;
+            this.btnAUTO.BackgroundImage = global::MissionPlanner.Controls.Properties.Resources.mode_auto_on;
+            this.btnAUTO.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAUTO.FlatAppearance.BorderSize = 0;
+            this.btnAUTO.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnAUTO.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnAUTO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAUTO.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F);
+            this.btnAUTO.ForeColor = System.Drawing.Color.Transparent;
+            this.btnAUTO.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnAUTO.Location = new System.Drawing.Point(108, 0);
+            this.btnAUTO.Name = "btnAUTO";
+            this.btnAUTO.Size = new System.Drawing.Size(121, 50);
+            this.btnAUTO.TabIndex = 8;
+            this.btnAUTO.TabStop = false;
+            this.btnAUTO.Tag = "custom";
+            this.btnAUTO.UseVisualStyleBackColor = false;
+            this.btnAUTO.Click += new System.EventHandler(this.btnAUTO_Click);
             // 
             // labelMode
             // 
             this.labelMode.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold);
             this.labelMode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(75)))), ((int)(((byte)(0)))));
             this.labelMode.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelMode.Location = new System.Drawing.Point(3, 11);
+            this.labelMode.Location = new System.Drawing.Point(5, 11);
             this.labelMode.Name = "labelMode";
-            this.labelMode.Size = new System.Drawing.Size(116, 29);
+            this.labelMode.Size = new System.Drawing.Size(97, 29);
             this.labelMode.TabIndex = 7;
             this.labelMode.Tag = "custom";
-            this.labelMode.Text = "ALT HOLD";
+            this.labelMode.Text = "ALTHOLD";
             this.labelMode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ModeControl
             // 
             this.BackColor = System.Drawing.SystemColors.WindowText;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnLOITER);
+            this.Controls.Add(this.btnAUTO);
             this.Controls.Add(this.labelMode);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "ModeControl";
-            this.Size = new System.Drawing.Size(365, 50);
+            this.Size = new System.Drawing.Size(350, 50);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnLOITER;
+        private System.Windows.Forms.Button btnAUTO;
         private System.Windows.Forms.Label labelMode;
     }
 }
