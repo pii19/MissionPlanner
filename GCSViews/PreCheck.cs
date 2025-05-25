b66d5c4720e8b7a5677bc39cfbb8d3c4c2835b1a
pii19 <piichan19@gmail.com>
2025-05-26 03:22:10 +0900

    add precheck control base

---
+++ GCSViews/PreCheck.cs
@@ -0,0 +1,39 @@
+﻿using MissionPlanner.Controls;
+using MissionPlanner.Properties;
+using MissionPlanner.Utilities;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MissionPlanner.GCSViews
+{
+    public partial class PreCheck : MyUserControl, IActivate
+    {
+        public PreCheck()
+        {
+            InitializeComponent();
+        }
+
+        public void Activate()
+        {
+            try
+            {
+                ;
+            }
+            catch
+            {
+            }
+
+            if (Program.WindowsStoreApp)
+            {
+                ;
+            }
+        }
+
+        private void PreCheck_Load(object sender, EventArgs e)
+        {
+            ;
+            //ThemeManager.ApplyThemeTo(richTextBox1);
+        }
+    }
+}
