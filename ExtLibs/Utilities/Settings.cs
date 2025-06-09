99c8eaf00dd25e3eb0b28f7cf8b29e04952965b2
pii19 <piichan19@gmail.com>
2025-06-09 18:14:22 +0900

    configをconfig.xmlに一本化

--- ExtLibs/Utilities/Settings.cs
+++ ExtLibs/Utilities/Settings.cs
@@ -44,7 +44,7 @@ namespace MissionPlanner.Utilities
         /// </summary>
         public static Dictionary<string, string> config = new Dictionary<string, string>();
 
-        public static string FileName { get; set; } = "fps_config.xml";
+        public static string FileName { get; set; } = "config.xml";
 
         public string this[string key]
         {
